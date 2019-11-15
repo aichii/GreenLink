078f91966f0bea38b8465377a7675b82c7f311da
Andy Weiss <andyweiss1982@gmail.com>
2019-11-15 16:28:49 -0500

    fiddle with timestamps and rename columns

--- db/migrate/20191115203443_create_fpl_accounts.rb
+++ db/migrate/20191115203443_create_fpl_accounts.rb
@@ -1,9 +1,9 @@
 class CreateFplAccounts < ActiveRecord::Migration[6.0]
   def change
     create_table :fpl_accounts do |t|
-      t.string :encrypted_username
-      t.string :encrypted_username
-      t.belongs_to :user_id, null: false, foreign_key: true
+      t.string :encrypted_username, null: false
+      t.string :encrypted_password, null: false
+      t.belongs_to :user, null: false, foreign_key: true
       t.string :zipcode
 
       t.timestamps
