acf2aefee3b7d65e3b5271c26e00e2fc16384b9a
Andres Mejias <aemabit@gmail.com>
2019-11-17 16:42:40 -0500

    add tip route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   root 'dashboards#show'
   resource :fpl_account, only: [:create, :destroy]
-  resources :challenges
+  resources :challenges, only: [:index]
+  resources :tips, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
