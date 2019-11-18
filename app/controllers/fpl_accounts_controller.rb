174569b6931677e5ac929261d54064f4aa07dab4
JamesKemp296 <jameskemp2796@yahoo.com>
2019-11-18 17:29:24 -0500

    changed view from dashboard to inddex, reroute to dashboard after fpl account creation

--- app/controllers/fpl_accounts_controller.rb
+++ app/controllers/fpl_accounts_controller.rb
@@ -1,15 +1,21 @@
 class FplAccountsController < ApplicationController
 
   def index
+ 
   end
+
   def create
-    @fpl_account = FplAccount.new(username: params[:username], password: params[:password], user: current_user)
-    @fpl_account.save
-    redirect_back fallback_location: root_url, notice: "Successfully logged in!"
+    fpl_account = FplAccount.new(username: params[:username], password: params[:password], user: current_user)
+    if fpl_account.save
+      redirect_to dashboard_url, fallback_location: root_url, notice: "Successfully logged in!"
+    else
+      redirect_to dashboard_url, fallback_location: root_url, alert: task.errors.full_messages.to_sentence.capitalize
+    end
   end
 
   def destroy
-    @fpl_account.destroy
-    redirect_back fallback_location: root_url, notice: "Account successfully deleted"
+    # @fpl_account = FplAccount.find(id: params[:id])
+    # @fpl_account.destroy
+    # redirect_back fallback_location: root_url, notice: "Account successfully deleted"
   end
 end
