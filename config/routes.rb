6b4b2edf2128b33c084a51513079b82fe9ac133a
Andy Weiss <andyweiss1982@gmail.com>
2019-11-19 15:47:11 -0500

    resolve conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-<<<<<<< HEAD
 
   root 'dashboards#show'
   resource :fpl_account, only: [:create, :destroy]
-=======
-  root 'dashboard#show'
->>>>>>> divide resoures for each route
   resources :challenges, only: [:index]
   resources :tips, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
