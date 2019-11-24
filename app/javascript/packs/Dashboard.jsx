d4f1b1496e7481a40bae1247205fe7c337b3bc01
JamesKemp296 <jameskemp2796@yahoo.com>
2019-11-24 15:44:01 -0500

    created layout react view and css

--- app/javascript/packs/Dashboard.jsx
+++ app/javascript/packs/Dashboard.jsx
@@ -2,8 +2,11 @@ import ReactOnRails from 'react-on-rails';
  
 import Dashboard from '../bundles/Dashboard/components/Dashboard';
 import Challenge from '../bundles/Challenge/components/Challenge';
+import Landing from '../bundles/Landing/components/Landing';
 
-// This is how react_on_rails can see the Search in the browser.
+// This is how react_on_rails can see the components in the browser.
 ReactOnRails.register({
-  Dashboard, Challenge
+  Dashboard,
+  Challenge,
+  Landing
 });
