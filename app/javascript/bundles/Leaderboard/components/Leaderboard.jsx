bd59827fa32ed50c3587d542939c41ef75697795
JamesKemp296 <jameskemp2796@yahoo.com>
2019-11-25 14:06:53 -0500

    created react component for leaderboard

---
+++ app/javascript/bundles/Leaderboard/components/Leaderboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default class Leaderboard extends React.Component {
+  render() {
+    return (
+      <main style={{ marginLeft: "100px" }}>
+        <h1>Hello from the leaderboard page</h1>
+      </main>
+      
+    )
+  }
+}
--- app/javascript/packs/Dashboard.jsx
+++ app/javascript/packs/Dashboard.jsx
@@ -3,10 +3,11 @@ import ReactOnRails from 'react-on-rails';
 import Dashboard from '../bundles/Dashboard/components/Dashboard';
 import Challenge from '../bundles/Challenge/components/Challenge';
 import Landing from '../bundles/Landing/components/Landing';
-
+import Leaderboard from '../bundles/Leaderboard/components/Leaderboard';
 // This is how react_on_rails can see the components in the browser.
 ReactOnRails.register({
   Dashboard,
   Challenge,
-  Landing
+  Landing,
+  Leaderboard
 });
