f1f2ea8a946829c2e7a5f976e8977555ccc096d2
JamesKemp296 <jameskemp2796@yahoo.com>
2019-11-25 14:12:14 -0500

    changed show to index action

--- app/controllers/leaderboards_controller.rb
+++
@@ -1,4 +0,0 @@
-class LeaderboardsController < ApplicationController
-  def show
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   root 'landing#show'
   resource :dashboard, only: [:show]
-  resource :leaderboard, only: [:show]
+  resources :leaderboard, only: [:index]
   resource :fpl_account, only: [:create, :destroy] do
     resource :sync, only: [:create]
   end
