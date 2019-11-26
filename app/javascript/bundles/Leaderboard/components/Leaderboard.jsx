24311c435287e23b0de5558d63799f8a83b257ff
JamesKemp296 <jameskemp2796@yahoo.com>
2019-11-26 15:10:17 -0500

    basic html wireframe

--- app/javascript/bundles/Leaderboard/components/Leaderboard.jsx
+++ app/javascript/bundles/Leaderboard/components/Leaderboard.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import axios from 'axios'
-import styles from './leaderboard.module.css'
 
 export default class Leaderboard extends React.Component {
   state={
@@ -13,42 +12,75 @@ export default class Leaderboard extends React.Component {
   }
 
   render() {
+    let rank = 4
     return (
-      <main id={styles.mainBox}>
-        <div className={styles.rankContainer}>
-          <div> 
-            <h1>All Zipcode Rank</h1>
-            <h2>Images go here</h2>
+      <main id="leaderboard-main">
+        <div className="rank-container">
+          <div className="rank-header"> 
+            <h1 className="leaderboard-title">
+              All Zipcode Rank
+            </h1>
+            <div className="rank-group">
+              <h2>02</h2>
+              <div className="rank-two-img">Image</div>
+              <h1>01</h1>
+              <div className="rank-one-img">Image</div>
+              <h3>03</h3>
+              <div className="rank-three-img">Image</div>
+            </div>
           </div>
-          <div className={styles.rankBox}>
+          <div className="rank-box">
             {
               this.state.users.map((user, i ) =>{
-                if (i < 8){
+                if (i < 5){
                   return(
-                    <h1 key={user.id}>
-                      {user.user_email}
-                    </h1> 
+                    <div className="rank-user-box" key={user.id}>
+                      <div>Image</div>
+                      <h1>{rank++}</h1>
+                      <h4>
+                        {user.user_email}
+                      </h4>
+                      <h3>
+                        {user.zipcode}
+                      </h3>
+                      <h2>PTS</h2>
+                    </div>
                   )
                 }
               })
             }
           </div>
         </div>
-        <div className={styles.rankContainer}>
-          <div>
-            <h1>My Network Rank</h1>
-            <h2>Images go here</h2>
+        <div className="rank-container">
+          <div className="rank-header">
+            <h1 className="leaderboard-title">
+              My Network Rank
+            </h1>
+            <div className="rank-group">
+              <h2>02</h2>
+              <div className="rank-two-img">Image</div>
+              <h1>01</h1>
+              <div className="rank-one-img">Image</div>
+              <h3>03</h3>
+              <div className="rank-three-img">Image</div>
+            </div>
           </div>
-          <div className={styles.rankBox}>
+          <div className="rank-box">
             {
               this.state.users.map((user, i ) =>{
-                if (i < 8){
+                if (i < 5){
                   return(
-                    <h1 
-                      key={user.id}
-                    >
-                      {user.user_email}
-                    </h1> 
+                    <div className="rank-user-box" key={user.id}>
+                      <div>Image</div>
+                      <h1>{rank++}</h1>
+                      <h4>
+                        {user.user_email}
+                      </h4>
+                      <h3>
+                        {user.zipcode}
+                      </h3>
+                      <h2>PTS</h2>
+                    </div>
                   )
                 }
               })
