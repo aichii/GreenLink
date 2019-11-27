3df1e22dc4cb15db2ca825331f6b56ac96dc6de4
Andres Mejias <aemabit@gmail.com>
2019-11-27 18:10:13 -0500

    change parameters to views

--- app/controllers/about_us_controller.rb
+++ app/controllers/about_us_controller.rb
@@ -1,5 +1,5 @@
 class AboutUsController < ApplicationController
-  
+  skip_before_action :authenticate_user!
   def show
   end
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resource :about_us, only: [:show]
 
   root 'landing#show'
   resource :dashboard, only: [:show]
@@ -7,7 +8,6 @@ Rails.application.routes.draw do
   resource :fpl_account, only: [:create, :destroy] do
     resource :sync, only: [:create]
   end
-  resource :about_us, only: [:show]
   resources :fpl_accounts, only: [:index]
   resource :profile, only: [:create, :destroy]
   resources :challenges, only: [:index]
