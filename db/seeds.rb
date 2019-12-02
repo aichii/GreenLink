73400544439ef57d9f5151e25e0004f7c81c0197
Andy Weiss <andyweiss1982@gmail.com>
2019-12-02 13:10:44 -0500

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -275,13 +275,13 @@ Challenge.create(
                 )
 
 # seed 100 users
-# seed 100 users
 response = HTTParty.get('https://randomuser.me/api/?results=100&seed=greenlink&inc=picture')
 
 100.times do |n|
-  user = User.create(email: Faker::Internet.email, password: "password")
+  email = Faker::Internet.email
+  user = User.create(email: email, password: "password")
   fpl_account = FplAccount.create!(
-                  username: Faker::Internet.email,
+                  username: email,
                   password: "password",
                   user: user,
                   zipcode: ["33132", "33024", "33126", "33141", "33139", "33140"].sample
